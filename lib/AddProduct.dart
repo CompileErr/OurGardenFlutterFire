71ee5fde98423bbd86a3cf1508221947bf4181a9
lexyball <lexyball@gmail.com>
2023-08-13 23:44:12 +1200

    updated AddProduct screen to have dropdown values in it

--- lib/AddProduct.dart
+++ lib/AddProduct.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class AddProduct extends StatelessWidget {
+class AddProduct extends StatefulWidget {
+  @override
+  _AddProductState createState() => _AddProductState();
+}
+
+class _AddProductState extends State<AddProduct> {
+  String dropdownValue = 'All regions';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add product'),
+        title: Text('Add a new product'),
       ),
       body: Center(
-        child: Text('This is the Add product screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DropdownButton<String>(
+                  value: dropdownValue,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  items: <String>['All regions', 'Northland', 'Waikato', 'Auckland','Wairarapa']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                Spacer(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
